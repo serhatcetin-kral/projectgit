f9d9389fdf442b514f70084dad95f994a13b49c1
serhat <serhat06.sc@gmail.com>
2020-12-15 21:08:13 -0800

    first commit from loal machine

---
+++ src/packagegit/Classgit.java
@@ -0,0 +1,12 @@
+package packagegit;
+
+public class Classgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git hub deneme");
+		
+	}
+
+}
